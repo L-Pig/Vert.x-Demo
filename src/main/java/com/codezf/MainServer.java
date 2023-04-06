4aeb0fbd0f12ab0e1d56321d9d973d57bdc6b88e
xiaozhu <17629065820@163.com>
2023-04-07 03:29:32 +0800

    test

--- src/main/java/com/codezf/MainServer.java
+++ src/main/java/com/codezf/MainServer.java
@@ -1,10 +1,19 @@
 package com.codezf;
 
-import io.vertx.core.AbstractVerticle;
-import io.vertx.rxjava.core.Vertx;
-import io.vertx.rxjava.core.http.HttpServer;
+import io.reactivex.rxjava3.core.Completable;
+import io.reactivex.rxjava3.core.Single;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.core.json.JsonObject;
+import io.vertx.rxjava3.core.AbstractVerticle;
+import io.vertx.rxjava3.core.Vertx;
+import io.vertx.rxjava3.core.http.HttpServer;
+import io.vertx.rxjava3.ext.web.Router;
+import io.vertx.rxjava3.ext.web.RoutingContext;
+import io.vertx.rxjava3.ext.web.handler.BodyHandler;
+import io.vertx.rxjava3.ext.web.handler.LoggerHandler;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-
 
 
 /**
@@ -15,21 +24,50 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class MainServer extends AbstractVerticle {
 
-
-    // Convenience method so you can run it in your IDE
     public static void main(String[] args) {
-
-        HttpServer httpServer = Vertx.vertx().createHttpServer();
-        httpServer
-                .requestStream().toObservable()
-                .subscribe(request -> request.response().end("Hello World!"));
-        httpServer.rxListen(9999).subscribe(success ->
-                        log.info("Http Server started on port " + httpServer.actualPort() + " successfully ")
-                );
-
-//                .requestHandler(request -> request.response().end("Hello World!"))
-//                .listen(9999)
-//                .onSuccess(server -> System.out.println("Http Server started on port " + server.actualPort() + " successfully "));
+        Vertx.vertx().deployVerticle(MainServer.class.getName());
     }
 
+    @Override
+    public Completable rxStart() {
+        HttpServer httpServer = vertx.createHttpServer();
+        Single<HttpServer> listen = httpServer
+
+                .requestHandler(scanRouter())
+                .listen(9999);
+        Disposable ignore = listen
+                .subscribe(onSuccess -> log.info("Http Server started on port " + httpServer.actualPort() + " successfully "));
+        return listen.ignoreElement();
+    }
+
+    public Router scanRouter() {
+        Router router = Router.router(vertx);
+        router.route().failureHandler(context -> {
+                    Throwable failure = context.failure();
+                    log.error("error", failure);
+                    context.json(new JsonObject()
+                            .put("code", 500)
+                            .put("msg", failure.getMessage())
+                            .put("data", null));
+                }).handler(LoggerHandler.create())
+                .handler(BodyHandler.create());
+        router.route().handler(this::logUri);
+        UserController.INSTANCE.pushRouter(router, vertx);
+        return router;
+    }
+
+    private void logUri(RoutingContext context) {
+        HttpMethod method = context.request().method();
+
+        JSON pojo = context.body().asPojo(JSON.class);
+        log.info("request method: {}, uri: {}, body: {}", method, context.request().uri(), pojo);
+        log.info("request method: {}, uri: {}", context.request().method(), context.request().uri());
+        context.next();
+    }
+
+    @Data
+    public static class JSON {
+        private String name;
+        private String password;
+    }
 }
---
+++ src/main/java/com/codezf/UserController.java
@@ -0,0 +1,50 @@
+package com.codezf;
+
+import io.vertx.core.json.JsonObject;
+import io.vertx.rxjava3.core.Vertx;
+import io.vertx.rxjava3.ext.web.Router;
+import io.vertx.rxjava3.ext.web.RoutingContext;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class UserController {
+
+    public static final UserController INSTANCE = new UserController();
+
+    public void pushRouter(Router router, Vertx vertx) {
+        Router subRouter = Router.router(vertx);
+        subRouter.get("/").handler(this::handleRoot);
+        subRouter.post("/login").handler(this::handleLogin);
+
+        router.route("/user/*").subRouter(subRouter);
+
+    }
+
+    private void handleRoot(RoutingContext context) {
+        System.out.println(context.request().uri());
+        JsonObject put = new JsonObject()
+                .put("code", 200)
+                .put("msg", "Hello World!")
+                .put("data", null);
+
+        context.json(put);
+    }
+
+    private void handleLogin(RoutingContext context) {
+
+        MainServer.JSON pojo = context.body().asPojo(MainServer.JSON.class);
+
+        if (pojo.getName() == null || pojo.getPassword() == null) {
+            context.response().setStatusCode(400).end();
+            return;
+        }
+        if (pojo.getName().equals("admin") && pojo.getPassword().equals("admin")) {
+            context.json(new JsonObject()
+                    .put("code", 200)
+                    .put("msg", "Hello World!")
+                    .put("data", new JsonObject()
+                            .put("username", pojo.getName())
+                            .put("password", pojo.getPassword())));
+        }
+    }
+}
